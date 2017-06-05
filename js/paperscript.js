e4d222fe9c24ecdbec0487b271790de566045391
Raghav Mathur <raghav3501@gmail.com>
2017-06-05 20:09:04 +0530

    added sections

--- js/paperscript.js
+++ js/paperscript.js
@@ -1,11 +1,6 @@
-
-var x = 0.9;
-
-
 $(document).ready(function(){
 	$('.main').on('mousewheel', function(updown){
 		if(updown.originalEvent.wheelDelta/120 > 0) {
-
 			$(".main").css({"transform":"scale(1)"})
 		}else{
 			$(".container").css({"top":"-100%"})
@@ -14,17 +9,27 @@ $(document).ready(function(){
 		}
 	})
 
-
-
-
-
 	$('.scup').on('mousewheel', function(updow){
 		if(updow.originalEvent.wheelDelta/120 > 0) {
 			$(".container").css({"top":"0"})
 			$(".scup").css({"transform":"scale(0.7)"})
 			$(".main").css({"transform":"scale(1)"})
 		}else{
-			
+			$(".container").css({"top":"-200%"})
+			$(".scup").css({"transform":"scale(0.7)"})
+			$(".products").css({"transform":"scale(1)"})
+		}
+	})
+
+	$('.products').on('mousewheel', function(updo){
+		if(updo.originalEvent.wheelDelta/120 > 0) {
+			$(".container").css({"top":"-100%"})
+			$(".products").css({"transform":"scale(0.7)"})
+			$(".scup").css({"transform":"scale(1)"})
+		}else{
+			/*$(".container").css({"top":"-300%"})
+			$(".products").css({"transform":"scale(0.7)"})
+			$(".scup").css({"transform":"scale(0.7)"})*/
 		}
 	})
 });
